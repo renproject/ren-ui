680c0fc7a132fde10aea7420b1c12e3ac8b796c6
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-11-22 18:28:35 +0100

    feat: exporting all icons

--- packages/icons/src/lib/icons.tsx
+++ packages/icons/src/lib/icons.tsx
@@ -1,3 +1,26 @@
+import { ReactComponent as RenBadger } from "./assets/renBADGER.svg";
+import { ReactComponent as RenBch } from "./assets/renBCH.svg";
+import { ReactComponent as RenBnb } from "./assets/renBNB.svg";
 import { ReactComponent as RenBtc } from "./assets/renBTC.svg";
+import { ReactComponent as RenBusd } from "./assets/renBUSD.svg";
+import { ReactComponent as RenComp } from "./assets/renCOMP.svg";
+import { ReactComponent as RenDai } from "./assets/renDAI.svg";
+import { ReactComponent as RenDgb } from "./assets/renDGB.svg";
+import { ReactComponent as RenDigi } from "./assets/renDIGI.svg";
+import { ReactComponent as RenDoge } from "./assets/renDOGE.svg";
+import { ReactComponent as RenEth } from "./assets/renETH.svg";
+import { ReactComponent as RenEurt } from "./assets/renEURT.svg";
+import { ReactComponent as RenFil } from "./assets/renFIL.svg";
+import { ReactComponent as RenFtm } from "./assets/renFTM.svg";
+import { ReactComponent as RenFtt } from "./assets/renFTT.svg";
+import { ReactComponent as RenLink } from "./assets/renLINK.svg";
+import { ReactComponent as RenLuna } from "./assets/renLUNA.svg";
+import { ReactComponent as RenMatic } from "./assets/renMATIC.svg";
+import { ReactComponent as RenRook } from "./assets/renROOK.svg";
+import { ReactComponent as RenSol } from "./assets/renSOL.svg";
+import { ReactComponent as RenUni } from "./assets/renUNI.svg";
+import { ReactComponent as RenUsdt } from "./assets/renUSDT.svg";
+import { ReactComponent as RenWbtc } from "./assets/renWBTC.svg";
+import { ReactComponent as RenZec } from "./assets/renZEC.svg";
 
-export { RenBtc }
+export { RenBadger, RenBch, RenBnb, RenBtc, RenBusd, RenComp, RenDai, RenDgb, RenDigi, RenDoge, RenEth, RenEurt, RenFil, RenFtm, RenFtt, RenLink, RenLuna, RenMatic, RenRook, RenSol, RenUni, RenUsdt, RenWbtc, RenZec }
