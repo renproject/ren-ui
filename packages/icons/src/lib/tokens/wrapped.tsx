c948d3acf6bc0c2d6204d8eafc4bcb1b5671cb3f
tomasz-rozmus <tomasz.rozmus@gmail.com>
2021-12-18 07:51:26 +0100

    chore: ren wrappers

--- packages/icons/src/lib/tokens/wrapped.tsx
+++ packages/icons/src/lib/tokens/wrapped.tsx
@@ -1,51 +1,65 @@
-import { ReactComponent as RenBadger } from "./assets/tokens/wrapped/renBADGER.svg";
-import { ReactComponent as RenBch } from "./assets/tokens/wrapped/renBCH.svg";
-import { ReactComponent as RenBnb } from "./assets/tokens/wrapped/renBNB.svg";
-import { ReactComponent as RenBtc } from "./assets/tokens/wrapped/renBTC.svg";
-import { ReactComponent as RenBusd } from "./assets/tokens/wrapped/renBUSD.svg";
-import { ReactComponent as RenComp } from "./assets/tokens/wrapped/renCOMP.svg";
-import { ReactComponent as RenDai } from "./assets/tokens/wrapped/renDAI.svg";
-import { ReactComponent as RenDgb } from "./assets/tokens/wrapped/renDGB.svg";
-import { ReactComponent as RenDigi } from "./assets/tokens/wrapped/renDIGI.svg";
-import { ReactComponent as RenDoge } from "./assets/tokens/wrapped/renDOGE.svg";
-import { ReactComponent as RenEth } from "./assets/tokens/wrapped/renETH.svg";
-import { ReactComponent as RenEurt } from "./assets/tokens/wrapped/renEURT.svg";
-import { ReactComponent as RenFil } from "./assets/tokens/wrapped/renFIL.svg";
-import { ReactComponent as RenFtm } from "./assets/tokens/wrapped/renFTM.svg";
-import { ReactComponent as RenFtt } from "./assets/tokens/wrapped/renFTT.svg";
-import { ReactComponent as RenLink } from "./assets/tokens/wrapped/renLINK.svg";
-import { ReactComponent as RenLuna } from "./assets/tokens/wrapped/renLUNA.svg";
-import { ReactComponent as RenMatic } from "./assets/tokens/wrapped/renMATIC.svg";
-import { ReactComponent as RenRook } from "./assets/tokens/wrapped/renROOK.svg";
-import { ReactComponent as RenSol } from "./assets/tokens/wrapped/renSOL.svg";
-import { ReactComponent as RenUni } from "./assets/tokens/wrapped/renUNI.svg";
-import { ReactComponent as RenUsdt } from "./assets/tokens/wrapped/renUSDT.svg";
-import { ReactComponent as RenWbtc } from "./assets/tokens/wrapped/renWBTC.svg";
-import { ReactComponent as RenZec } from "./assets/tokens/wrapped/renZEC.svg";
+import { ReactComponent as RenAvax } from "../assets/tokens/wrapped/renAVAX.svg";
+import { ReactComponent as RenBadger } from "../assets/tokens/wrapped/renBADGER.svg";
+import { ReactComponent as RenBch } from "../assets/tokens/wrapped/renBCH.svg";
+import { ReactComponent as RenBnb } from "../assets/tokens/wrapped/renBNB.svg";
+import { ReactComponent as RenBtc } from "../assets/tokens/wrapped/renBTC.svg";
+import { ReactComponent as RenBusd } from "../assets/tokens/wrapped/renBUSD.svg";
+import { ReactComponent as RenComp } from "../assets/tokens/wrapped/renCOMP.svg";
+import { ReactComponent as RenCrv } from "../assets/tokens/wrapped/renCRV.svg";
+import { ReactComponent as RenDai } from "../assets/tokens/wrapped/renDAI.svg";
+import { ReactComponent as RenDigi } from "../assets/tokens/wrapped/renDIGI.svg";
+import { ReactComponent as RenDoge } from "../assets/tokens/wrapped/renDOGE.svg";
+import { ReactComponent as RenEth } from "../assets/tokens/wrapped/renETH.svg";
+import { ReactComponent as RenEurt } from "../assets/tokens/wrapped/renEURT.svg";
+import { ReactComponent as RenFil } from "../assets/tokens/wrapped/renFIL.svg";
+import { ReactComponent as RenFtm } from "../assets/tokens/wrapped/renFTM.svg";
+import { ReactComponent as RenFtt } from "../assets/tokens/wrapped/renFTT.svg";
+import { ReactComponent as RenKnc } from "../assets/tokens/wrapped/renKNC.svg";
+import { ReactComponent as RenLink } from "../assets/tokens/wrapped/renLINK.svg";
+import { ReactComponent as RenLuna } from "../assets/tokens/wrapped/renLUNA.svg";
+import { ReactComponent as RenMatic } from "../assets/tokens/wrapped/renMATIC.svg";
+import { ReactComponent as RenMim } from "../assets/tokens/wrapped/renMIM.svg";
+import { ReactComponent as RenRen } from "../assets/tokens/wrapped/renREN.svg";
+import { ReactComponent as RenRook } from "../assets/tokens/wrapped/renROOK.svg";
+import { ReactComponent as RenSol } from "../assets/tokens/wrapped/renSOL.svg";
+import { ReactComponent as RenSushi } from "../assets/tokens/wrapped/renSUSHI.svg";
+import { ReactComponent as RenUni } from "../assets/tokens/wrapped/renUNI.svg";
+import { ReactComponent as RenUsdt } from "../assets/tokens/wrapped/renUSDT.svg";
+import { ReactComponent as RenUsdc } from "../assets/tokens/wrapped/renUSDC.svg";
+import { ReactComponent as RenWbtc } from "../assets/tokens/wrapped/renWBTC.svg";
+import { ReactComponent as RenWeth } from "../assets/tokens/wrapped/renWETH.svg";
+import { ReactComponent as RenZec } from "../assets/tokens/wrapped/renZEC.svg";
 
 export {
+  RenAvax,
   RenBadger,
   RenBch,
   RenBnb,
   RenBtc,
   RenBusd,
   RenComp,
+  RenCrv,
   RenDai,
-  RenDgb,
   RenDigi,
   RenDoge,
   RenEth,
   RenEurt,
   RenFil,
   RenFtm,
+  RenKnc,
   RenFtt,
   RenLink,
   RenLuna,
   RenMatic,
+  RenMim,
+  RenRen,
   RenRook,
   RenSol,
+  RenSushi,
   RenUni,
   RenUsdt,
+  RenUsdc,
   RenWbtc,
+  RenWeth,
   RenZec,
 };
