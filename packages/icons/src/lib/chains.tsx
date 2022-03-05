4616359b1bdcfe399b129563b0e4024539162ec1
tomasz-rozmus <tomasz.rozmus@gmail.com>
2022-03-05 20:03:08 +0100

    feat: coinbase wallet, fantom rename

--- packages/icons/src/lib/chains.tsx
+++ packages/icons/src/lib/chains.tsx
@@ -1,8 +1,17 @@
+import { ReactComponent as Arbitrum } from "./assets/chains/Arbitrum.svg";
 import { ReactComponent as Avalanche } from "./assets/chains/Avalanche.svg";
 import { ReactComponent as BinanceSmartChain } from "./assets/chains/BinanceSmartChain.svg";
 import { ReactComponent as Ethereum } from "./assets/chains/Ethereum.svg";
-import { ReactComponent as Phantom } from "./assets/chains/Phantom.svg";
+import { ReactComponent as Fantom } from "./assets/chains/Fantom.svg";
 import { ReactComponent as Polygon } from "./assets/chains/Polygon.svg";
 import { ReactComponent as Solana } from "./assets/chains/Solana.svg";
 
-export { Avalanche, BinanceSmartChain, Ethereum, Phantom, Polygon, Solana };
+export {
+  Avalanche,
+  Arbitrum,
+  BinanceSmartChain,
+  Ethereum,
+  Fantom,
+  Polygon,
+  Solana,
+};
--- packages/icons/src/lib/wallets.tsx
+++ packages/icons/src/lib/wallets.tsx
@@ -1,7 +1,15 @@
 import { ReactComponent as BinanceChainWallet } from "./assets/wallets/BinanceChainWallet.svg";
+import { ReactComponent as CoinbaseWallet } from "./assets/wallets/CoinbaseWallet.svg";
 import { ReactComponent as MetaMask } from "./assets/wallets/MetaMask.svg";
 import { ReactComponent as MyEtherWallet } from "./assets/wallets/MyEtherWallet.svg";
 import { ReactComponent as PhantomWallet } from "./assets/wallets/PhantomWallet.svg";
 import { ReactComponent as Sollet } from "./assets/wallets/Sollet.svg";
 
-export { MetaMask, BinanceChainWallet, PhantomWallet, MyEtherWallet, Sollet };
+export {
+  MetaMask,
+  BinanceChainWallet,
+  CoinbaseWallet,
+  PhantomWallet,
+  MyEtherWallet,
+  Sollet,
+};
