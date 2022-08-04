97ca0761a3dba541365b4dd01c0c5e5ad1b749d8
Tomasz Rozmus <tomasz.rozmus@gmail.com>
2022-08-04 23:12:16 +0200

    fix: kava, glmr icons correnction, changing chain naming convention

--- packages/icons/src/lib/chains.tsx
+++ packages/icons/src/lib/chains.tsx
@@ -1,25 +1,25 @@
-import { ReactComponent as Arbitrum } from "./assets/chains/Arbitrum.svg";
-import { ReactComponent as Avalanche } from "./assets/chains/Avalanche.svg";
+import { ReactComponent as ArbitrumChain } from "./assets/chains/Arbitrum.svg";
+import { ReactComponent as AvalancheChain } from "./assets/chains/Avalanche.svg";
 import { ReactComponent as BinanceSmartChain } from "./assets/chains/BinanceSmartChain.svg";
-import { ReactComponent as Catalog } from "./assets/chains/Catalog.svg";
-import { ReactComponent as Ethereum } from "./assets/chains/Ethereum.svg";
-import { ReactComponent as Fantom } from "./assets/chains/Fantom.svg";
-import { ReactComponent as Kava } from "./assets/chains/Kava.svg";
-import { ReactComponent as Moonbeam } from "./assets/chains/Moonbeam.svg";
-import { ReactComponent as Optimism } from "./assets/chains/Optimism.svg";
-import { ReactComponent as Polygon } from "./assets/chains/Polygon.svg";
-import { ReactComponent as Solana } from "./assets/chains/Solana.svg";
+import { ReactComponent as CatalogChain } from "./assets/chains/Catalog.svg";
+import { ReactComponent as EthereumChain } from "./assets/chains/Ethereum.svg";
+import { ReactComponent as FantomChain } from "./assets/chains/Fantom.svg";
+import { ReactComponent as KavaChain } from "./assets/chains/Kava.svg";
+import { ReactComponent as MoonbeamChain } from "./assets/chains/Moonbeam.svg";
+import { ReactComponent as OptimismChain } from "./assets/chains/Optimism.svg";
+import { ReactComponent as PolygonChain } from "./assets/chains/Polygon.svg";
+import { ReactComponent as SolanaChain } from "./assets/chains/Solana.svg";
 
 export {
-  Avalanche,
-  Arbitrum,
+  AvalancheChain,
+  ArbitrumChain,
   BinanceSmartChain,
-  Catalog,
-  Ethereum,
-  Fantom,
-  Kava,
-  Moonbeam,
-  Optimism,
-  Polygon,
-  Solana,
+  CatalogChain,
+  EthereumChain,
+  FantomChain,
+  KavaChain,
+  MoonbeamChain,
+  OptimismChain,
+  PolygonChain,
+  SolanaChain,
 };
